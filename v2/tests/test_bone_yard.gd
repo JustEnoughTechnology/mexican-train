d15102a22f280abb469583873649876ca6958933
Rob Wolfe <rob@portwallistechnologies.com>
2025-01-27 04:21:41 -0500

    drag and drop mods

--- v2/tests/test_bone_yard.gd
+++ v2/tests/test_bone_yard.gd
@@ -1,5 +1,8 @@
 extends Node2D
-# Called when the node enters the scene tree for the first time.
+
+@onready var is_dragging:=false
+@onready var current_domino:Domino 
+
 func _ready() -> void:
 	$BoneYard.set_size(get_window().size)
 	$BoneYard.populate(12,false) 
@@ -8,23 +11,22 @@ func _ready() -> void:
 
 func _process(_delta: float) -> void:
 	pass
-
-func raycast_check_for_domino():
-	var space_state = get_world_2d().direct_space_state
-	var parameters = PhysicsPointQueryParameters2D.new()
-	parameters.position = get_global_mouse_position()
-	parameters.collide_with_areas = true
-	parameters.collision_mask = 1
-	var result = space_state.intersect_point(parameters)
-	print (result)
 	
 func _on_bone_yard_domino_left_pressed(p_domino: Domino) -> void:
 	p_domino.show_dots() # Replace with function body.
 
 func _on_bone_yard_gui_input(event: InputEvent) -> void:
-	pass
+	if event is InputEventMouseMotion and is_dragging:
+		current_domino.position += event.relative
 	
-
 func _on_bone_yard_domino_right_pressed(p_domino: Domino) -> void:
-	pass # Replace with function body.
-/
+	p_domino.mouse_filter = Control.MOUSE_FILTER_IGNORE
+	is_dragging = true
+	current_domino = p_domino
+	p_domino.highlight(true)
+
+func _on_bone_yard_domino_right_released(p_domino: Domino) -> void:
+	is_dragging = false
+	current_domino = null
+	p_domino.mouse_filter = Control.MOUSE_FILTER_PASS
+	p_domino.highlight(false)
