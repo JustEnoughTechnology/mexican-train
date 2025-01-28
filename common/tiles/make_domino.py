2d8401ad5bfe114f4ddc6c2e4a7287a0e36a5263
Rob Wolfe <rob@portwallistechnologies.com>
2025-01-28 06:33:23 -0500

    new 2d dominos

--- common/tiles/make_domino.py
+++ common/tiles/make_domino.py
@@ -1,19 +1,28 @@
-base = "C:\\development\\godot\\mexican-train\\common\\tiles"
-p_dots = 3
 
+def mkdomino(l,r) :
+    base = "C:\\development\\godot\\mexican-train-1\\common\\tiles"
+    l1 = layer("L1")
+    r1 = rect((0,0),(81*px,39*px),fill="black")
+    r1.translate(-r1.bounding_box().minimum)
+    l1.append(r1)
+    left = objects_from_svg_file(base+f"\\domino-{l}.svg",False)
+    right = objects_from_svg_file(base+f"\\domino-{r}.svg",False)
+    left = left[0]
+    right = right[0]
+    l1.append(left)
+    left.translate(-left.bounding_box().minimum)
+    l1.append(right)
+    right.translate(-right.bounding_box().minimum )
+    right.translate((42.0*px,0))
+    save_file(base+f"\\domino-{l}-{r}.svg")
+    l1.remove()
+    left.remove()
+    right.remove()
+    r1.remove()
+
+p_dots = 12
 for l in range (0,p_dots+1):
-    for i in all_shapes():
-        i.remove()
-        for r in range(0,l+1):
-            left = objects_from_svg_file(base+f"\\domino-{l}.svg",False)
-            right = objects_from_svg_file(base+f"\\domino-{r}.svg",False)
-            left = left[0]
-            right = right[0]
-            left.translate(-left.bounding_box().minimum)
-            right.translate(-right.bounding_box().minimum )
-            right.translate((42.0*px,0))
-            save_file(base+f"\\domino-{l}-{r}.svg")
-        
-    
+    for r in range(0,l+1):
+        mkdomino(l,r)        
     
 
