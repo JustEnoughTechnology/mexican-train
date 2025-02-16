d16f58e91f63778316e56445a75c106294ff6c96
rwolfe+1-at-812206943367 <rob@portwallistechnologies.com>
2025-02-16 11:16:56 -0500

    refactor of playerhand

--- v2/game_board/player_hand.gd
+++ v2/game_board/player_hand.gd
@@ -1,22 +1,22 @@
 class_name PlayerHand extends ColorRect
-@onready var domino_container := $VBoxContainer/HFlowContainer
+
+@onready var domino_container := $HandContainer/bg/DominoContainer
 @onready var my_player : Player
+@onready var my_label := $HandContainer/bg/Label
 
 var d_scene : PackedScene = preload("res://v2/game_pieces/domino.tscn")
 var count:int 
 
-signal domino_clicked(p_domino:Domino)
-signal domino_right_clicked(p_domino:Domino)
 signal domino_dropped(p_domino:Domino)
 
 func get_domino_count() -> int:
 	return domino_container.get_child_count()
 	
 func set_label_text(p_text:String):
-	$VBoxContainer/Label.text = p_text
+	my_label.text = p_text
 
 func get_label_text()->String:
-	return $VBoxContainer/Label.text
+	return my_label.text
 	
 func sort_ascending(domino1:Domino,domino2:Domino)->bool:
 	return domino1.get_dots() < domino2.get_dots()
@@ -35,43 +35,19 @@ func sort(s:GameState.Sort):
 		_:
 			pass		
 	
-func shuffle():
-	var d_array = domino_container.get_children()
-	d_array.shuffle()
-	
-	for d in range(0,d_array.size()):
-		domino_container.move_child(d_array[d],d)
-
 func add_domino(p_domino:Domino)->void:
 	p_domino.reparent(domino_container)
 	
 func get_domino(i:int)-> Domino:
-	return $VBoxContainer/HFlowContainer.get_child(i)
+	return domino_container.get_child(i)
 	
 func move_domino(p_domino:Domino,p_dest) ->void:
 	p_domino.reparent(p_dest)
 		
-func populate(p_dots:int,p_face_up:bool):
-	var d:Domino
-	for i:int in range(0,p_dots+1):
-		for j:int in range(0,i+1):
-			d = d_scene.instantiate()
-			d.name = "Domino_"+str(i)+"_"+str(j)
-			domino_container.add_child(d)
-			d.domino_clicked.connect(_on_domino_clicked)
-			d.domino_right_clicked.connect(_on_domino_right_clicked)
-			d.set_dots(i,j)
-			d.show_dots(p_face_up)
-			
-func _on_domino_clicked(p_domino:Domino):
-	domino_clicked.emit(p_domino)
-	
-func _on_domino_right_clicked(p_domino:Domino):
-	domino_right_clicked.emit(p_domino)
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _ready() -> void:
 	domino_dropped.connect(add_domino)
+	my_label.text = self.name + " Hand"
 	
 func _process(_delta: float) -> void:
 	pass
--- v2/tests/test_player_hand.gd
+++ v2/tests/test_player_hand.gd
@@ -2,23 +2,16 @@ extends Node2D
 var current_domino:Domino
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
+	for d:Domino in $dominos.get_children():
+		d.set_dots(randi_range(0,12),randi_range(0,12))
+		d.mouse_left_pressed.connect(_on_domino_mouse_left_pressed)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
+func _on_domino_mouse_left_pressed(p_domino: Domino) -> void:
+	$PlayerHand.add_domino(p_domino) # Replace with function body.
 
-func _on_player_hand_gui_input(event: InputEvent) -> void:
-	if event.is_action_pressed("mouse_left") :
-		if $Container.get_child_count() > 0:
-			current_domino = $Container.get_child(0)
-			$PlayerHand.add_domino(current_domino)
-	elif event.is_action_pressed("mouse_right"):
-		if $PlayerHand.get_domino_count() >0 :
-			current_domino = $PlayerHand.get_domino(-1)
-			$PlayerHand.move_domino(current_domino,$Container)
 			
 					
