a6778f43988f984ee57ddb0018cd510734311d41
rwolfe+1-at-812206943367 <rob@portwallistechnologies.com>
2025-03-06 21:19:59 -0500

    3d dominos

--- v2/game_board/game_board.gd
+++ v2/game_board/game_board.gd
@@ -4,7 +4,9 @@ class_name GameBoard extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	#$BoneYard.set_size(get_window().size*0.75)
+	$BoneYard.populate(12,false) 
+	$BoneYard.shuffle()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
