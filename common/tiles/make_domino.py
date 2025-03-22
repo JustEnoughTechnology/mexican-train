3241ba81bab5720795dc8f1013dc8c5f30060452
rwolfe+1-at-812206943367 <rob@portwallistechnologies.com>
2025-03-22 10:18:48 -0400

    18 by 18 dominos finished up

--- common/tiles/make_domino.py
+++ common/tiles/make_domino.py
@@ -1,12 +1,12 @@
-
-def mkdomino(l,r) :
-    base = "C:\\development\\godot\\mexican-train-1\\common\\tiles"
+def mkdomino(l_parm,r_parm) :
+    print(f"Making domino {l_parm}-{r_parm}")
+    base = "C:\\development\\godot\\mexican-train\\common\\tiles"
     l1 = layer("L1")
     r1 = rect((0,0),(81*px,39*px),fill="black")
     r1.translate(-r1.bounding_box().minimum)
     l1.append(r1)
-    left = objects_from_svg_file(base+f"\\domino-{l}.svg",False)
-    right = objects_from_svg_file(base+f"\\domino-{r}.svg",False)
+    left = objects_from_svg_file(base+f"\\domino-{l_parm}.svg",False)
+    right = objects_from_svg_file(base+f"\\domino-{r_parm}.svg",False)
     left = left[0]
     right = right[0]
     l1.append(left)
@@ -14,15 +14,17 @@ def mkdomino(l,r) :
     l1.append(right)
     right.translate(-right.bounding_box().minimum )
     right.translate((42.0*px,0))
-    save_file(base+f"\\domino-{l}-{r}.svg")
+    save_file(base+f"\\domino-{l_parm}-{r_parm}.svg")
+    print (f"Removing domino {l_parm}-{r_parm}")
+    print(f"{l1}")
     l1.remove()
-    left.remove()
-    right.remove()
-    r1.remove()
-
-p_dots = 12
-for l in range (0,p_dots+1):
-    for r in range(0,l+1):
-        mkdomino(l,r)        
-    
-
+    #print(f"{left}")
+    #left.remove()
+    #print(f"{right}")
+    #right.remove()
+    #print(f"{r1}")
+    #r1.remove()
+p_dots = 18
+for ll in range (0,p_dots+1):
+    for rr in range(0,ll+1):
+        mkdomino(ll,rr)  
