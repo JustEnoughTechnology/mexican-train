957d048aecd015132e4340a303f79ebfa815fe5a
rwolfe+1-at-812206943367 <rob@portwallistechnologies.com>
2025-05-26 10:35:17 -0400

    major refactor of structure

--- scripts/game_board/train.gd
+++
@@ -1,5 +0,0 @@
-class_name Train extends DominoGameContainer
-
-func _ready() -> void:
-	super._ready()
-	self.set_label_text("A Player Train")
--- scripts/players/player.gd
+++ scripts/players/player.gd
@@ -7,10 +7,7 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-<<<<<<<< HEAD:scripts/players/player.gd
 @warning_ignore("unused_parameter")
 func _process(delta: float) -> void:
-========
-func _process(_delta: float) -> void:
->>>>>>>> 8997d63e58dfdd559b24b196f66b00a42b1399ee:scenes/players/player.gd
+
 	pass
